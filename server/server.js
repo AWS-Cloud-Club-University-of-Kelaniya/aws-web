ef248c36cb53febca486af97e9d4ebc732878064
blackdevil <madushanchathuranghe192@gmail.com>
2025-04-18 12:35:29 +0530

    server update

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 
 
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '.env') }); 
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 const app = express();
 const cors = require('cors');
