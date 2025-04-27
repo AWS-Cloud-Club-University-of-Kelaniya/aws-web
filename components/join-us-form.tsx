e21d7a6f5eac4de3e2a13ed6ddf924fb030de3fb
blackdevil <madushanchathuranghe192@gmail.com>
2025-04-27 15:57:37 +0530

    join update

--- components/join-us-form.tsx
+++ components/join-us-form.tsx
@@ -85,7 +85,7 @@ export default function JoinUsForm() {
     },
   });
 
-  async function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {//${process.env.NEXT_PUBLIC_API_URL}
     console.log(values);
     setIsSubmitting(true);
     try {
